7f2879b732cf17c582ecda93fda0c2c232d0665c
YeongJin Gwon <40593455+newhar@users.noreply.github.com>
2022-01-02 07:05:02 +0000

    greedy - 큰수의 법칙, 1이 될때까지

--- main.py
+++ main.py
@@ -1,30 +1,12 @@
-# 15:28~15:30
-def solution(n,k) :
-  cnt = 0
-  while(n != 1) :
-    if( n % k == 0 ) :
-      n = n//k
-    else :
-      n -= 1
-    cnt+=1
-  print(cnt)
-  return cnt
+# 15:52 ~ 16:02
+nums = input()
 
-def solution2(n,k) :
-  cnt = 0
-  while True :
-    target = (n//k) * k
-    cnt += (n-target)
-    n = target
-    if( n < k ) :
-      break
-    n //= k
-    cnt += 1
-  
-  cnt += (n-1)
-  return cnt
+l = int(nums[0])
+for idx in range(1, len(nums)) :
+  r = int(nums[idx])
+  if(l <= 1 or r <= 1) :
+    l += r
+  else :
+    l *= r
 
-
-n,k = map(int, input().split())
-result = solution(n,k)
-result2 = solution(n,k)
+print(l)
