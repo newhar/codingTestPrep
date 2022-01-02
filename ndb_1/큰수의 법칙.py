22fc42dd6d84b1af6d7a4e08f795ed49657ce62c
YeongJin Gwon <40593455+newhar@users.noreply.github.com>
2022-01-02 07:36:02 +0000

    greedy 기본 완료

--- ndb_1/큰수의 법칙.py
+++ ndb_1/큰수의 법칙.py
@@ -1,1 +1,12 @@
-print("hello")
+# 15:52 ~ 16:02
+nums = input()
+
+l = int(nums[0])
+for idx in range(1, len(nums)) :
+  r = int(nums[idx])
+  if(l <= 1 or r <= 1) :
+    l += r
+  else :
+    l *= r
+
+print(l)
